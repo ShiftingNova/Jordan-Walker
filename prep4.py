a904a2b93a5f47a759fca096271b018453c21b3b
ShiftingNova <89542928+ShiftingNova@users.noreply.github.com>
2021-08-30 13:09:14 -0700

    Update prep4.py

--- prep4.py
+++ prep4.py
@@ -1,8 +1,8 @@
-feet = int(input("Number of feet:"))
+feet = int(input("Number of feet:\n"))
 inch = feet*12
 meter = round(feet*0.3048 , 3)
 rod=round(feet/16.5 , 1)
-print("Inches: "+str(inch))
+print("\nInches: "+str(inch))
 print("Meter: "+str(meter))
 print("Rods: "+str(rod))
 
