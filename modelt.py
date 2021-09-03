dfe71679b7138376cef7bc15b6dadfe76bc92b0b
ShiftingNova <89542928+ShiftingNova@users.noreply.github.com>
2021-09-03 12:59:54 -0700

    Update modelt.py

--- modelt.py
+++ modelt.py
@@ -1,11 +1,10 @@
-length= int(input("Model-T length:\n"))
+length = int(input("Model-T length:\n"))
 print(".-----------"+"-"*length+".")
 print("| ### ||  ###" + "#" * length + "\ ")
 print("| ### ||  ####"+"#"*length+"\.")
 print("D     ||  "+" "*length+"<>    |------+")
 print("|  ______      "+" "*length+"/______ |")
 print(" \/ /..\ \_____"+"_"*length+"/ /..\ \|")
-print("    \__/         "+" "*length+"\__/")
-# Jordan Walker
-# CSC 110
-# 9/1/21
+print("    \__/         " + " " * length + "\__/")
+# Jordan Walker CSC 110 9/1/21
+
