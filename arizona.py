6d0ab543e665ee3381a7c635db85201f9ab5888a
ShiftingNova <89542928+ShiftingNova@users.noreply.github.com>
2021-09-07 14:26:52 -0700

    Update arizona.py

--- arizona.py
+++ arizona.py
@@ -1,12 +1,12 @@
 side = int(input("USS Arizona outer width:\n"))
 inner = int(input("USS Arizona inner width:\n"))
 height = int(input("USS Arizona tower height:\n"))
-print("                     "+ " " * side +"|" + " " * (inner*2) + "|")
-print("              " + " " * side + "    |##$" + " " * (inner*2) + "$##|" + ("\n                 " + " "*side + "   ##" + " " * (inner*2) + "##") * height)
+print(("                     "+ " " * side +"|" + " " * (inner*2) + "|\n")*int(((side+inner)/7))+("              " + " " * side + "    |##$" + " " * (inner*2) + "$##|" + ("\n                 " + " "*side + "   ##" + " " * (inner*2) + "##") * height))
 print("               " + " " * side +  "   #..#" + " " * (inner*2) + "#..#")
 print("           "+ " " * side + "\----. #..#" + "." * (inner*2) + "#..# .----/")
 print("     " +" "* side + "\ ***|_|    |#..#"+ "-#" * (inner) + "#..#|    |_|*** /")
 print(".____" + "_" * side + "_|____          _" + "." * (inner*2) + "_          ____|_"+"_" * side + "..")
 print("`---." + "     "+" "*(side*2) + "        " + " "*(inner*2) + "                      --\.")
 print("<<#\___________________________" + "_" * (side*2) + "_____"+"_" * (inner*2) + "________\ ")
+
 #Jordan Walker CSC110 2021
