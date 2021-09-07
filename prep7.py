86b6a510103c990959d842026c6ea804b5a713d6
ShiftingNova <89542928+ShiftingNova@users.noreply.github.com>
2021-09-07 16:17:04 -0700

    Create prep7.py

---
+++ prep7.py
@@ -0,0 +1,11 @@
+size = input("Drink Size:\n")
+type = input("Drink type:\n")
+s2=0
+t2 = 0
+if size.lower() =="large":
+    s2 = 2
+else:
+    s2 = 1
+if type.lower() == "regular":
+    t2 = 3
+print(str(50*t2*s2)+" calories")
