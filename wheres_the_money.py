9d16bfc68db61b7a213d17ae2e45af0d86ab4a3e
ShiftingNova <89542928+ShiftingNova@users.noreply.github.com>
2021-09-13 20:43:55 -0700

    Update wheres_the_money.py

--- wheres_the_money.py
+++ wheres_the_money.py
@@ -32,22 +32,21 @@ left = salary-(morgage*12) - (bills*12) - (food*52) - (travel) - (salary * (tax 
 taxed = salary * (tax / 100.00)
 if taxed > 75000:
     taxed == 75000.00
-count = len("| mortgage/rent | $  " + str(morgage*12) + ".00" + " |  " + str(morgage*12/salary*100) + "% | " + "#" * int(morgage*12/salary*100))
+count = len("| mortgage/rent | $  " + str(format(morgage*12,'9,.2f')) + " |  " + str(round(morgage*12/salary*100,1) )+ "% | " + "#" * int(morgage*12/salary*100))
 print("-"*count)
 print("See the financial breakdown below, based on a salary of $" + str(salary))
 print("-"*count)
-print("| mortgage/rent | $  " + str(morgage*12) + ".00" + " |  " + str(morgage*12/salary*100) + "% | " + "#" * int(morgage*12/salary*100))
-print("|         bills | $   " + str(bills*12) + ".00" + " |   " + str(bills*12/salary*100) + "% | " + "#" * int(bills*12/salary*100))
-print("|          food | $  "+str(food*52) + ".00" + " |  " + str(food*52/salary*100) + "% | " + "#" * int(food*52/salary*100))
-print("|        travel | $   " + str(travel) + ".00" + " |   " + str(travel/salary*100) + "% | " + "#" * int(travel/salary*100))
-print("|           tax | $  " + str(taxed) + "0 |  " + str(tax) +".0% | " + "#" * tax)
-print("|         extra | $  " + str(left) +"0 |  " + str(round(left/salary*100,1)) + "% | " + "#" * int(left/salary*100))
+print("| mortgage/rent | $  " + str(format(morgage*12,'9,.2f')) + " |  " + str(round(morgage*12/salary*100,1) )+ "% | " + "#" * int(morgage*12/salary*100))
+print("|         bills | $  " + str(format(bills*12,'9,.2f')) + " |   " + str(round(bills*12/salary*100,1)) + "% | " + "#" * int(bills*12/salary*100))
+print("|          food | $  "+str(format(food*52,'9,.2f')) + " |  " + str(round(food*52/salary*100,1)) + "% | " + "#" * int(food*52/salary*100))
+print("|        travel | $  " + str(format(travel,'9,.2f')) + " |   " + str(round(travel/salary*100,1)) + "% | " + "#" * int(travel/salary*100))
+print("|           tax | $  " + str(format(taxed,'9,.2f')) + " |  " + str(round(tax,2)) +".0% | " + "#" * tax)
+print("|         extra | $  " + str(format(left,'9,.2f')) +" |  " + str(round(left/salary*100,1)) + "% | " + "#" * int(left/salary*100))
 print("-"*count)
 if (salary * (tax / 100)) >= 75000:
     print(">>> TAX LIMIT REACHED <<<")
 if left < 0:
     print(">>> WARNING: DEFICIT <<<")
-
 
 ### 
 ### Author: Jordan Walker
