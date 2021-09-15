ae981960b3e274bed64199078bba5501b5036617
ShiftingNova <89542928+ShiftingNova@users.noreply.github.com>
2021-09-14 20:05:52 -0700

    Create prep9.py

---
+++ prep9.py
@@ -0,0 +1,10 @@
+name = input("Enter factorial to calculate:\n")
+sum = 1
+if name.isnumeric():
+    name = int(name)
+    for i in range(1, name + 1):
+        sum = i * sum
+    print(str(name)+" factorial = "+str(sum))
+    # Jordan Walker
+    # CSC 110
+    # thsi was quick i dont know why i named the variavle "name" brain was tired i guess
