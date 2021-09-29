15645d82ba4c4c805212f7cfedac11a3c89a11ed
ShiftingNova <89542928+ShiftingNova@users.noreply.github.com>
2021-09-29 12:01:45 -0700

    Update prep13.py

--- prep13.py
+++ prep13.py
@@ -5,4 +5,4 @@ def count_characters(sequence, letter_A, letter_B):
         if(sequence[i]) == letter_A or sequence[i] == letter_B:
             count+=1
         i+=1
-    print(letter_A, "and", letter_B, "appeared", count, "times in the string", sequence)
+    print("'" + letter_A+"'"+" and '"+letter_B+"' appeared "+str(count)+ " times in the string" +sequence)
