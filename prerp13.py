b3468a08887541b036ed40f5000b15efd33b6be3
ShiftingNova <89542928+ShiftingNova@users.noreply.github.com>
2021-09-29 11:56:03 -0700

    Create prerp13.py

---
+++ prerp13.py
@@ -0,0 +1,8 @@
+def count_characters(sequence, letter_A, letter_B):
+    i = 0
+    count = 0
+    while i<len(sequence):
+        if(sequence[i]) == letter_A or sequence[i] == letter_B:
+            count+=1
+        i+=1
+    print(letter_A, "and", letter_B, "appeared", count, "times in the string", sequence)
